b80a05ac040f40d831e5fdda5f01c1833961fd1b
Anu Mittal <anu22mittal@gmail.com>
2021-02-16 18:50:16 +0800

    Initial Commit

---
+++ Day26/Day26App.swift
@@ -0,0 +1,17 @@
+//
+//  Day26App.swift
+//  Day26
+//
+//  Created by Anu Mittal on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Day26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
